6495b94dd8278317e4cbdcf2282b6319d6fc073b
thelonehegelian <thelonehegelian@gmail.com>
2023-05-05 18:40:19 +0100

    Add comments

--- typescript/src/data-structures/trees/binaryTree.ts
+++ typescript/src/data-structures/trees/binaryTree.ts
@@ -18,27 +18,32 @@ class BinaryTree {
   }
 
   insert(value: number) {
+    // create a new node with the value passed to the function
     const newNode = new Node(value);
 
+    // if there is no root property on the tree, set the root to be the newly created node
     if (this.root === null) {
       this.root = newNode;
       return this;
     }
 
     let current: Node | null = this.root;
-
+    // loop through the tree and find the correct position for the new node
     while (true) {
+      // value is less than the value of the current node, go left
       if (value < current.value) {
         if (current.left === null) {
           current.left = newNode;
           return this;
         }
         current = current.left;
+        // value is greater than the value of the current node, go right
       } else if (value > current.value) {
         if (current.right === null) {
           current.right = newNode;
           return this;
         }
+
         current = current.right;
       } else {
         return undefined;
