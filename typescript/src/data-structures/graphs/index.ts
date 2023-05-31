2fd54f9710f9eb31bdcc0f1d54ec158d47e6538a
thelonehegelian <thelonehegelian@gmail.com>
2023-05-31 22:35:37 +0100

    Add bfs graph traversal

--- typescript/src/data-structures/graphs/index.ts
+++ typescript/src/data-structures/graphs/index.ts
@@ -67,4 +67,48 @@ class Graph {
       v.edges = v.edges.filter(edge => edge.endVertex.name !== vertex.name);
     });
   }
+
+  // @todo add recursive dfs traversal
+  // @note this is depth first search 
+  // the function takes a start vertex 
+  dfsTraversal(startVertex: Vertex) {
+    // keep track of visited vertices
+    let visited = {};
+    let result = [];
+    let stack = [];
+    stack.push(startVertex);
+    visited[startVertex.name] = true;
+    while (stack.length) {
+      let currentVertex = stack.pop();
+      result.push(currentVertex);
+      // for each edge in the current vertex
+      currentVertex.edges.forEach(edge => {
+        // if the end vertex of the edge has not been visited add it to the stack
+        if (!visited[edge.endVertex.name]) {
+          stack.push(edge.endVertex);
+          visited[edge.endVertex.name] = true;
+        }
+      });
+    }
+  }
+
+  // @todo add recursive bfs traversal
+  // @note this is breadth first search
+  bfsTraversal(startVertex: Vertex) {
+    let visited = {};
+    let result = [];
+    let queue = [];
+    queue.push(startVertex);
+    visited[startVertex.name] = true;
+    while (queue.length) {
+      let currentVertex = queue.shift();
+      result.push(currentVertex);
+      currentVertex.edges.forEach(edge => {
+        if (!visited[edge.endVertex.name]) {
+          queue.push(edge.endVertex);
+          visited[edge.endVertex.name] = true;
+        }
+      });
+    }
+  }
 }
